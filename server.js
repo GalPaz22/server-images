256a2afb037266892098c05d172a0d7ee5e743ca
GalPaz22 <galpaz2210@gmail.com>
2025-03-28 17:45:10 +0300

    first commit

--- server.js
+++ server.js
@@ -1035,7 +1035,7 @@ app.post("/recommend", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
